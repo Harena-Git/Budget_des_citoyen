a84e7721c6154bd59cf74ee709b96db88b9a416c
NyVoary <93976218+NyVoary@users.noreply.github.com>
2025-10-23 20:10:31 +0300

    flight okey

--- config/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-Flight::register('db', 'PDO', array(
-    'pgsql:host=localhost;port=5432;dbname=test',
-    'postgres',        // ton utilisateur PostgreSQL
-    ''       // ton mot de passe
-), function ($db) {
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-});
