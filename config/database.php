532fe32ad20720f2680ba1a2bd8d06b24fb6affe
Harena-Git <faniriantsoaharena@gmail.com>
2025-10-23 20:49:19 +0100

    Mise en place

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 Flight::register('db', 'PDO', array(
     'pgsql:host=localhost;port=5432;dbname=test',
     'postgres',        // ton utilisateur PostgreSQL
-    ''       // ton mot de passe
+    'harena'       // ton mot de passe
 ), function ($db) {
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 });
